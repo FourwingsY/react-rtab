47ef813c3a3b4bb23d9df9db18b0e1c98a059fc6
FourwingsY <fourwingsy@gmail.com>
2016-02-23 22:26:29 +0900

    update building system

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
 const autoprefixer = require('autoprefixer')
 const precss = require('precss')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const fs = require('fs')
+const path = require('path')
+
+var entries = {'bundle': './examples/index.js'}
+fs.readdirSync("themes").forEach((fileName) => {
+  var regex = /(.+)\.pcss$/
+  if (regex.test(fileName)) {
+    var name = regex.exec(fileName)[1]
+    entries[name] = './themes/' + fileName
+  }
+})
 
 module.exports = {
-  entry: './src/Rtab.js',
+  entry: entries,
   output: {
-    filename: './dist/react-rtab.js'
+    filename: '[name].js',
+    path: './dist'
   },
   module: {
     loaders: [
-      {test: /\.js$/, loader: 'babel', exclude: /node_modules/, include: /src/},
-      {test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader'},
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /\.p?css$/, loader: ExtractTextPlugin.extract('style-loader','css-loader!postcss-loader')},
     ]
   },
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['', '.js', '.css', '.pcss'],
+    alias: {
+      'react-rtab': path.join(__dirname, 'src')
+    }
   },
+  plugins: [
+    new ExtractTextPlugin("[name].css")
+  ],
   postcss: function () {
     return [autoprefixer, precss];
   }
